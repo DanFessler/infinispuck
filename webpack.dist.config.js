420a5c5596b7617ba7da52c2d8dd0da050b0a455
Dan Fessler <fezzinate@gmail.com>
2021-03-26 23:06:34 -0700

    fixing build

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,38 +1,57 @@
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const SRC = path.resolve(__dirname, "src");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/main.js',
-    output: {
-        path: __dirname + '/dist/',
-        filename: './js/main.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            server: { baseDir: ['dist'] },
-            files: ['./dist/*'],
-            notify: false
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            favicon: 'favicon.ico',
-            template: 'src/index.html'
-        })
+  mode: "development",
+  entry: "./src/js/main.js",
+  output: {
+    path: __dirname + "/dist/",
+    filename: "./js/main.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.(wav|png)$/,
+        include: SRC,
+        loader: "file-loader",
+        exclude: /(node_modules|bower_components)/,
+        options: {
+          name: "images/[name].[ext]",
+        },
+      },
     ],
-    watch: false,
-    devtool: 'source-map'
-}
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 3000,
+      server: { baseDir: ["dist"] },
+      files: ["./dist/*"],
+      notify: false,
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      favicon: "favicon.ico",
+      template: "src/index.html",
+    }),
+  ],
+  watch: false,
+  devtool: "source-map",
+};
